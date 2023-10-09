556bdbf8547837b62ea192d3b1c98e0493ed9633
AgusMaff <102746223+AgusMaff@users.noreply.github.com>
2023-10-09 17:23:24 -0300

    Update alu_top.v

--- alu_top.v
+++ alu_top.v
@@ -10,33 +10,30 @@ module alu_top#(
     input btnC,
     input btnL,
     input btnR,
-    output [OPERAND_SIZE:0]LED
+    output [OPERAND_SIZE-1:0]LED
 );
     
     reg [OPERAND_SIZE-1:0]dato_a;
     reg [OPERAND_SIZE-1:0]dato_b;
     reg [OP_CODE_SIZE-1:0]op_code;
 
+    alu_logic ALU(.dato_a(dato_a),.dato_b(dato_b),.op_code(op_code),.o_resultado(LED));
+
     always @(posedge CLK100MHZ)
     begin
         if(btnL == 1)
             dato_a <= sw;
-            //flags[0] = 1'b1; 
     end
     
     always @(posedge CLK100MHZ)
     begin
         if(btnC == 1)
-            dato_b <= sw;
-            //flags[1] = 1'b1;        
+            dato_b <= sw;      
     end
     
     always @(posedge CLK100MHZ)
     begin
         if(btnR == 1)
-            op_code <= sw;
-            //flags[2] = 1'b1;        
+            op_code <= sw;     
     end
-
-    alu_logic ALU(.dato_a(dato_a),.dato_b(dato_b),.op_code(op_code),.o_resultado(LED));
 endmodule
