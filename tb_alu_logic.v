0de38536eab83c777810c79080e60e8c5f1d83e2
AgusMaff <102746223+AgusMaff@users.noreply.github.com>
2023-10-10 13:46:01 -0300

    Update tb_alu_logic.v
    Agregue un par de random tests para probar

--- tb_alu_logic.v
+++ tb_alu_logic.v
@@ -9,38 +9,60 @@ module tb_alu_logic();
     reg btnA;
     reg btnB;
     reg btnOP;
+    wire [OPERAND_SIZE-1:0]LED;
     
-    alu_logic DUT(.i_clk(t_clk),.i_switches(t_switches),.i_btn_A(btnA),.i_btn_B(btnB),.i_btn_OP(btnOP),.o_resultado(o_resultado));
+    alu_top DUT(.CLK100MHZ(t_clk),.sw(t_switches),.btnL(btnA),.btnC(btnB),.btnR(btnOP),.LED(LED));
     
     initial begin
-    t_clk = 1'b0;
-    btnA = 0;
-    btnB = 0;
-    btnOP = 0;
-    t_switches=8'b00000000;
-    #5
-    t_switches=8'b00001000;
-    btnA = 0; 
-    btnB = 1;
-    btnOP = 0;
-    #5
-    t_switches=8'b00000100;
-    btnA = 1; 
-    btnB = 0;
-    btnOP = 0;
-    #5
-    t_switches=6'b100111;
-    btnA = 0; 
-    btnB = 0;
-    btnOP = 1;
-    #5
-    btnOP = 0;
-    #20
+        t_clk = 0;
+        t_switches = 8'b00000000;
+        btnA = 0;
+        btnB = 0;
+        btnOP = 0;
+        #5
+        //t_switches = 8'b01001001;
+        t_switches = $random % 256;
+        btnA = 1;
+        btnB = 0;
+        btnOP = 0;
+        #5
+        //t_switches = 8'b01001000;
+        t_switches = $random % 256;
+        btnA = 0;
+        btnB = 1;
+        btnOP = 0;
+        #5
+        t_switches = 6'b100100;
+        btnA = 0;
+        btnB = 0;
+        btnOP = 1;
+        #5
+        t_switches = 8'b00000000;
+        btnA = 0;
+        btnB = 0;
+        btnOP = 0;
+        #1
+        btnOP = 1;
+        #5
+        t_switches = $random % 256;
+        btnA = 1;
+        btnB = 0;
+        btnOP = 0;
+        #5
+        t_switches = $random % 256;
+        btnA = 0;
+        btnB = 1;
+        btnOP = 0;
+        #5
+        t_switches = 6'b100101;
+        btnA = 0;
+        btnB = 0;
+        btnOP = 1;        
     $stop;      
     end
     
     always begin
         #1
-        t_clk = ~t_clk;
-    end 
+        t_clk = ~(t_clk);
+    end
 endmodule
